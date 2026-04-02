2a40a8a9c015f79c033ffcb206b93e770917a016
DsoAfrique <theafricancode1@gmail.com>
2026-04-02 21:52:59 +0200

    update

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 
 // 🧩 Middleware
 app.use(cors({
-  origin: 'https://www.vexolead.com',
+  origin: ['https://www.vexolead.com','https://vexolead.com'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
